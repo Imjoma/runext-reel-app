bdf83dc65e32b3755bc6d8b90062e29d08412418
Imjoma <33196540+Imjoma@users.noreply.github.com>
2023-08-05 19:14:58 +0800

    added seo

---
+++ app/configs/seo.js
@@ -0,0 +1,18 @@
+export const developer = "Joma Ipio";
+export const title = "R U Next";
+
+export const seo = {
+  applicationName: title,
+  creator: developer,
+  keywords: [
+    `${title} Website`,
+    `${title} Web Application`,
+    `${title} Survival Show`,
+    `${title} Promotion`,
+    `${title} Promotional Website`,
+    `${title} Website`,
+    `${title} JTBC`,
+    `${title} Be:lift Lab`,
+    `${title} Belift`,
+  ],
+};
--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,13 @@ import "./globals.css";
 
 import IconContextProvider from "./context/IconContext";
 
+import { seo } from "./configs";
+
 export const metadata = {
-  title: "RU:Next",
+  title: "R U Next Web Application",
   description:
     "R U Next? (알 유 넥스트?) is an upcoming 2023 girl group reality survival show created by Belift Lab. It is scheduled to premiere on June 30, 2023 and broadcast every Friday at 8:50 p.m. KST on JTBC and will follow the process of creating a new girl group.",
+  ...seo,
 };
 
 export default function RootLayout({ children }) {
@@ -15,6 +18,7 @@ export default function RootLayout({ children }) {
       <body>
         <IconContextProvider>
           <Topbar />
+
           {children}
         </IconContextProvider>
       </body>
